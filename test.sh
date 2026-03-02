b9ed4419740b6263747c9508e91080541cac9613
jondatkins <122027005+jondatkins@users.noreply.github.com>
2026-03-02 12:23:30 +0000

    Fixing test script

--- test.sh
+++ test.sh
@@ -3,6 +3,6 @@ go run . reset
 # Expecting exit code: 0
 go run . register kahya
 # Expecting exit code: 0
-go run . addfeed "Hacker News RSS" "https://hnrss.org/newest"
+go run . addfeed "Ycombinator Hacker News RSS" "https://news.ycombinator.com/rss"
 # Expecting exit code: 0
-go agg . 5s
+go run . agg 5s
